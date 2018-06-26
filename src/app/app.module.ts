e99f3f9d0e78a53f5edfc33a33aba11dddb1d8cc
dd270386pai <dd270386pai@gmail.com>
2018-06-26 09:22:46 +0300

    fix for forgot and login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'forgot-password', component: ForgotPasswordComponent},
   { path: '**', component: NotFoundComponent }
 ];
